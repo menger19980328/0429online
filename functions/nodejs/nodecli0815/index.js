56bda7b7d5ea4a9682b2f9b3213b5716fb9c954d
mengxianghui.1998.bytedance.com <mengxianghui.1998@bytedance.com>
2025-04-29 15:20:29 +0800

    [init] synchronize basic code

---
+++ functions/nodejs/nodecli0815/index.js
@@ -0,0 +1,21 @@
+// 通过 NPM dependencies 成功安装 NPM 包后此处可引入使用
+// 如安装 linq 包后就可以引入并使用这个包
+// const linq = require("linq");
+
+/**
+ * @param {Params}  params     自定义参数
+ * @param {Context} context    上下文参数，可通过此参数下钻获取上下文变量信息等
+ * @param {Logger}  logger     日志记录器
+ *
+ * @return 函数的返回数据
+ */
+module.exports = async function (params, context, logger) {
+  // 日志功能
+  for
+  (let i = 0; i < 10000; i++) {
+ logger.info(i)
+  }
+  
+  var faasenv = process.env.KFaaSType
+  logger.info(faasenv)
+}
